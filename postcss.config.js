1760b78581251f26ec598dc9da6672437b11a35a
Sebastian Nunez <02snunez@gmail.com>
2022-10-09 22:33:22 -0400

    Initial configuration for Monaco text editor

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    autoprefixer: {}
+  }
+};
+
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    // https://purgecss.com/configuration.html#options
+    content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+    css: [],
+    whitelistPatternsChildren: [/monaco-editor/], // so it handles .monaco-editor .foo .bar
+    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+  }
+];
